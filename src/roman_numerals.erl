89aa7accbf14ee52387aec48625f23204d46f1f8
Mårten Gustafson <marten.gustafson@gmail.com>
2011-03-13 23:31:56 +0100

    Cosmetics

--- src/roman_numerals.erl
+++ src/roman_numerals.erl
@@ -19,25 +19,24 @@
 -define(VALUES, [?ONES, ?TENS, ?HUNDREDS]).
 
 convert(Number) when Number > 0, Number =< 3000 ->
-    Numbers = to_int_list(integer_to_list(Number)),
-    BackwardsRoman = to_roman(?VALUES, lists:reverse(Numbers)),
+    Numbers = toIntList(integer_to_list(Number)),
+    BackwardsRoman = toRoman(?VALUES, lists:reverse(Numbers)),
     lists:flatten(lists:reverse(BackwardsRoman)).
 
-to_int_list([Head|Rest])    -> [list_to_integer([Head])|to_int_list(Rest)];
-to_int_list([])             -> [].
+toIntList([Head|Rest]) -> [list_to_integer([Head])|toIntList(Rest)];
+toIntList([])          -> [].
     
-to_roman([Values|NextValues], [Number|NextNumbers]) -> [lookup(Number, Values)|to_roman(NextValues, NextNumbers)];
-to_roman([], [Count|_Empty]) when Count > 0 -> ["M"|to_roman([], [Count - 1])];
-to_roman(_, _) -> [].
+toRoman([Values|NextValues], [Number|NextNumbers]) -> [lookup(Number, Values)|toRoman(NextValues, NextNumbers)];
+toRoman([], [Count|_]) when Count > 0              -> ["M"|toRoman([], [Count - 1])];
+toRoman(_, _)                                      -> [].
 
-lookup(0, _Values) -> [];
-lookup(Number, Values) -> lists:nth(Number, Values).
+lookup(0, _)            -> [];
+lookup(Number, Values)  -> lists:nth(Number, Values).
 
 % Demo
-examples() ->
-    examples([1234, 1990, 2008]).
+examples() -> examples([1, 10, 100, 1000, 12, 123, 1234, 1990, 2008]).
 
 examples([Number|Samples]) ->
-    io:format("~w  = ~s~n", [Number, convert(Number)]),
+    io:format("~w \t= ~s~n", [Number, convert(Number)]),
     examples(Samples);
 examples([]) -> ok.
